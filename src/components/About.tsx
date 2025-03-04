596b0cffcc81c6c9ee0ad1bc86952ead2a053046
dev-gaitano <dev.gaitano@gmail.com>
2025-03-04 20:52:13 +0300

    Made About paragraph more mobile responsive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,8 +9,8 @@ const About = () => {
           loading="lazy"
         />
       </div>
-      <div className="absolute md:bottom-32 max-md:top-12 left-60 max-lg:left-12 md:translate-y-[-50%] w-full">
-        <p className="gold-gradient w-[540px] max-md:w-[375px] text-xl max-md:text-xs">
+      <div className="absolute md:bottom-32 max-md:top-12 left-60 max-lg:left-10 md:translate-y-[-50%] w-full">
+        <p className="gold-gradient w-[540px] max-md:w-[350px] text-xl max-md:text-xs">
           <span className="inline text-7xl max-md:text-6xl float-start mr-1 font-other">
             E
           </span>
@@ -26,7 +26,7 @@ const About = () => {
         <p className="text-primary mt-4 max-md:mt-2 text-xl max-md:text-xs">
           ~ Aulgah Nato
         </p>
-        <button className="border border-primary text-primary text-xs px-6 py-3 mt-8 max-md:mt-4 transition-all ease-out duration-500 hover:border-gray-600 hover:text-gray-600 hover:px-3 hover:py-2">
+        <button className="border border-primary text-primary text-xs max-md:text-xxs px-6 py-3 mt-8 max-md:mt-4 transition-all ease-out duration-500 hover:border-gray-600 hover:text-gray-600 hover:px-3 hover:py-2">
           MORE ABOUT US
         </button>
       </div>
